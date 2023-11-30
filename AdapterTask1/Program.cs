859c42f47dc7fe963dafc5abbd420119d09af114
= <=>
2023-11-30 23:07:51 +0400

    adapter task1

---
+++ AdapterTask1/Program.cs
@@ -0,0 +1,31 @@
+﻿Adaptee adaptee = new Adaptee();
+ITarget target = new Adapter(adaptee);
+target.Request();
+
+public interface ITarget
+{
+    void Request();
+}
+
+public class Adaptee
+{
+    public void SpecificRequest()
+    {
+        Console.WriteLine("Adaptee's specific request");
+    }
+}
+
+public class Adapter : ITarget
+{
+    private Adaptee adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        this.adaptee = adaptee;
+    }
+
+    public void Request()
+    {
+        adaptee.SpecificRequest();
+    }
+}
